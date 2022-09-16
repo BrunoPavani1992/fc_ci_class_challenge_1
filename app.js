1640cdda7cb3450f826ba8d9f8c84066774f9b98
Bruno Pavani <brunopavani1992@gmail.com>
2022-09-16 16:12:05 -0300

    test

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ function Sum(number1 = 0, number2 = 0) {
   return number1 + number2;
 }
 
+
 test('Sum test', (t) => {
   assert.equal(Sum(5, 5), 10);
 });
+
 
 console.log(`The sum is ${Sum(5, 5)}`);
